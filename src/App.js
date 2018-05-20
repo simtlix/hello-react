fd2d3b51657922ad255951a409ddd38a6411689d
claudiogonzalez <claudio@simtlix.com>
2018-05-20 15:30:06 -0300

    Agregando la lista a App

--- src/App.js
+++ src/App.js
@@ -30,9 +30,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </button>
+        
+        <AlbumCardList />
 
         
       </div>
